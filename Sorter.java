4449540b06190ce137e1cade971c0c752e851df7
AliAlrebdi <alialrebdi@outlook.com>
2023-02-07 17:20:44 +0300

    "Added selection sort"

--- Sorter.java
+++ Sorter.java
@@ -18,4 +18,17 @@ public class Sorter {
             numArray[i] = temp; 
         } 
     } 
+
+    public static void insertionSort(int array[]) {  
+        int n = array.length;  
+        for (int j = 1; j < n; j++) {  
+            int key = array[j];  
+            int i = j-1;  
+            while ( (i > -1) && ( array [i] > key ) ) {  
+                array [i+1] = array [i];  
+                i--;  
+            }  
+            array[i+1] = key;  
+        }  
+    }
 }
